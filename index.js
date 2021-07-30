3fa679403e4d3875d5e35642cd135cdc68856e88
David Baker <dave@matrix.org>
2021-07-31 00:28:02 +0100

    do stuff

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function main() {
+    try {
+        const myToken = core.getInput('ghToken');
+        const octokit = github.getOctokit(myToken);
+
+        await octokit.rest.issues.createComment({
+            owner: github.context.owner,
+            repo: github.context.repo,
+            github.context.payload.number,
+            "boop",
+        }); 
+
+        const payload = JSON.stringify(github.context, undefined, 2)
+        console.log(`The event payload: ${payload}`);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+
+main();
