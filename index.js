152e5358a0c13ceaffa159109635e348c354e0d1
David Baker <dbkr@users.noreply.github.com>
2021-08-13 12:49:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,20 @@ const github = require('@actions/github');
 
 async function main() {
     try {
-        core.setFailed('insufficient llamas');
+        github.getOctokit().checks.create({
+            ...github.context.repo,
+            head_sha: context.pull_request.head.sha,
+            name: "testy / TestJob",
+            status: "completed",
+            conclusion: "action_required",
+            details_url: "https://www.google.com/",
+            output: {
+                title: "the title",
+                summary: "summery",
+                text: "the text",
+            },
+        });
+        //console.log(JSON.stringify(context);
         const myToken = core.getInput('ghToken');
         const octokit = github.getOctokit(myToken);
 
